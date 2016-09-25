3a4f660098198ad7488c30e5de4287706a2b8f1c
William Laeder <codylaeder@gmail.com>
2016-09-25 19:54:43 -0400

    Created workbook structure

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,28 @@ pub use zip::read::{ZipArchive,ZipFile};
 use std::io::prelude::*;
 use std::fs::{File, OpenOptions};
 
+pub struct WorkSheet {
+    name: String,
+    data: String
+}
+impl WorkSheet {
+    pub fn new(mut a: ZipFile) -> Result<WorkSheet,ZipError> {
+        let mut x = WorkSheet {
+            name: a.name().to_string(),
+            data: unsafe{ ::std::mem::uninitialized()}
+        };
+        let mut s = String::with_capacity(4000);
+        match a.read_to_string(&mut s) {
+            Ok(_) => { },
+            Err(e) => return Err(ZipError::Io(e))
+        };
+        x.data = s;
+        Ok(x)
+    }
+}
 
-pub fn open<'a>(path: &str) -> Result<Vec<String>,ZipError> {
+
+pub fn open(path: &str) -> Result<Vec<WorkSheet>,ZipError> {
     let f: File = match OpenOptions::new().read(true).write(false).open(path) {
         Ok(x) => x,
         Err(e) => return Err(ZipError::Io(e))
@@ -18,14 +38,12 @@ pub fn open<'a>(path: &str) -> Result<Vec<String>,ZipError> {
     let mut x = 0usize;
     loop {
         match arc.by_index(x) {
-            Ok(mut y) => {
-                let mut s = String::with_capacity(4000);
-                match y.read_to_string(&mut s) {
-                    Ok(_) => { },
-                    Err(e) => return Err(ZipError::Io(e))
-                };
-                v.push(s);
-                x += 1;
+            Ok(y) => match WorkSheet::new(y) {
+                Err(e) => return Err(e),
+                Ok(z) => {
+                    v.push(z);
+                    x += 1;
+                }
             },
             Err(ZipError::FileNotFound) => break,
             Err(e) => return Err(e)
